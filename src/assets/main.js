bb6f70e2c6ab6c49f37cb8c6fae8ee00b8bfd1c0
Nico <nicolasmm89@gmail.com>
2017-11-24 17:56:17 +0100

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,46 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer.value == '' && attempt.value == ''){
+      setHiddenFields();
+    }
+
+    if (!validateInput(input.value)){
+      return false;
+    } else {
+      attempt++
+    }
 }
 
 //implement new functions here
+function setHiddenFields (){
+ answer = Math.floor(Math.random()*10000);
+ answer.toString();
+ while(answer.length < 4) {
+   answer ='0'+answer;
+ }
+ attempt = 0;
+
+}
+
+
+function setMessage (labelText){
+  let message = document.getElementById('message');
+  let newLabel = document.createElement('label');
+  newLabel.setAttribute('for','message');
+  newLabel.innerHTML = labelText;
+  var parentDiv = message.parentNode;
+  parentDiv.insertBefore(newLabel,message);
+}
+
+function validateInput(parameter){
+  if (parameter.length == 4) {
+    return true;
+  } else {
+    setMessage('Guesses must be exactly 4 characters long.');
+    return false;
+  }
+}
+
+/*$(document).ready(function(){
+});*/
