7160c5f35fa372a82460c762377f800977326a94
Nico <nicolasmm89@gmail.com>
2017-11-24 21:18:27 +0100

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,36 +4,41 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (answer.value == '' && attempt.value == ''){
+    if (answer.value == '' || attempt.value == ''){
       setHiddenFields();
     }
 
     if (!validateInput(input.value)){
-      return false;
+      return;
     } else {
-      attempt++
+      attempt.value++
+    }
+    if(getResults(input.value)){
+      setMessage("You Win! :)");
+      showAnswer(true);
+      showReplay();
+    } else if (attempt.value >=10){
+      setMessage("You Lose! :(")
+      showAnswer(false);
+      showReplay();
+    } else {
+      setMessage("Incorrect, try again.");
     }
 }
 
 //implement new functions here
 function setHiddenFields (){
- answer = Math.floor(Math.random()*10000);
- answer.toString();
- while(answer.length < 4) {
-   answer ='0'+answer;
+ answer.value = Math.floor(Math.random()*10000).toString();
+ while(answer.value.length < 4) {
+   answer.value ='0'+answer.value;
  }
- attempt = 0;
-
+ attempt.value = 0;
 }
 
 
 function setMessage (labelText){
-  let message = document.getElementById('message');
-  let newLabel = document.createElement('label');
-  newLabel.setAttribute('for','message');
-  newLabel.innerHTML = labelText;
-  var parentDiv = message.parentNode;
-  parentDiv.insertBefore(newLabel,message);
+  document.getElementById('message').innerHTML = labelText;
+
 }
 
 function validateInput(parameter){
@@ -45,5 +50,44 @@ function validateInput(parameter){
   }
 }
 
-/*$(document).ready(function(){
-});*/
+
+function getResults(p){
+  var newContent = '<div class="row"><span class="col-md-6">' + p + '</span><div class="col-md-6">';
+
+  console.log('Actual answer is: '+answer.value+". Which is type: "+ typeof answer+"and length: "+ answer.length);
+  console.log("User's attempt is: "+p+". Which is type: "+ typeof p+"and length: "+ p.length);
+  var counter = 0;
+  for (var i = 0; i < p.length; i++ ){
+      if( answer.value.charAt(i) == p.charAt(i)) {
+         counter++;
+         newContent += '<span class="glyphicon glyphicon-ok"></span>';
+      } else if (answer.value.indexOf(p.charAt(i)) > -1) {
+        newContent += '<span class="glyphicon glyphicon-transfer"></span>';
+      } else{
+        newContent += '<span class="glyphicon glyphicon-remove"></span>';
+      }
+  }
+    newContent +=  '</div></div>';
+    document.getElementById('results').innerHTML += newContent;
+    console.log("pasamos");
+
+    if(counter == 4) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+function showAnswer(won){
+  document.getElementById('code').innerHTML = answer.value;
+  if(won){
+    document.getElementById('code').className += " success";
+  } else {
+    document.getElementById('code').className += " failure";
+  }
+}
+
+function showReplay(){
+  document.getElementById("guessing-div").style.display='none';
+  document.getElementById("replay-div").style.display="block";
+}
